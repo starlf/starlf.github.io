2aecef9408ec94590d8a09295a627acb6386058b
starlf <31914861+starlf@users.noreply.github.com>
2025-02-11 14:37:15 +0800

    Update docs.tsx
    markdown文件展示页面

--- src/pages/docs.tsx
+++ src/pages/docs.tsx
@@ -1,9 +1,32 @@
-const DocsPage = () => {
+// starlf.github.io/src/pages/docs/[docName].tsx
+import React, { useEffect, useState } from 'react';
+import ReactMarkdown from 'react-markdown';
+import rehypeRaw from 'rehype-raw';
+import remarkGfm from 'remark-gfm';
+import fs from 'fs';
+import path from 'path';
+
+const DocPage: React.FC<{ docName: string }> = ({ docName }) => {
+  const [markdownContent, setMarkdownContent] = useState<string>('');
+
+  useEffect(() => {
+    const filePath = path.join(__dirname, '..', 'files', docName);
+    fs.readFile(filePath, 'utf8', (err, data) => {
+      if (err) {
+        console.error('Error reading file:', err);
+      } else {
+        setMarkdownContent(data);
+      }
+    });
+  }, [docName]);
+
   return (
-    <div>
-      <p>This is umi docs.</p>
+    <div className="markdown-content">
+      <ReactMarkdown rehypePlugins={[rehypeRaw]} remarkPlugins={[remarkGfm]}>
+        {markdownContent}
+      </ReactMarkdown>
     </div>
   );
 };
 
-export default DocsPage;
+export default DocPage;
